f02fdfab084578293342474310e6a8a58d52f1a1
krishnahexaware <46746750+krishnahexaware@users.noreply.github.com>
2019-02-22 17:59:58 +0530

    Add files via upload

---
+++ new/Java/Tc.java
@@ -0,0 +1,11 @@
+public class Tc {
+    public static void main(String[] args) {
+        double b=12.5;
+        int i =(int)b;
+
+        int x = 12;
+        long x1=x;
+        double y1 = x;
+
+    }
+}
